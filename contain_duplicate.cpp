bfaabe679bf7a24c644960e1317d615f11ecbea3
gaurabshrestha991-tech <gaurabshrestha991@gmail.com>
2026-07-08 22:23:34 +0545

    Add files via upload

---
+++ contain_duplicate.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+
+            if(seen.find(num) != seen.end()) {
+                return true;
+            }
+
+            seen.insert(num);
+        }
+
+        return false;
+
+    }
+
+int main ()
+{
+    vector<int> nums = {1,2,3,1};
+
+    if(containsDuplicate(nums))
+    {
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+
+    return 0;
+}
